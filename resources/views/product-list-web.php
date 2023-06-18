a50cf53c16e498fdad5618ebd2fec4d826f4029c
Aliasyahira01 <Aliasyahira0612@gmail.com>
2023-06-18 18:54:12 +0800

    Product-list Alia
    product-list web & mobile

---
+++ resources/views/product-list-web.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.scss"/>
+    <title>Product list</title>
+</head>
+<body>
+    <table>
+        <tr colspan="2" style="bgcolor: #556B2F; text-align: center">
+            <h1 style="color: #804444; face: verdana; text-align: center;">Faculty</h1>
+        </tr>
+        <tr>
+            <button class="bubbly-button">Click me!</button>
+        </tr>
+    </table>  
+</body>
+<script type="text/javascript" src="javascript.js"></script>
+</html>
