53969282df4d26f4d51b6881b6fac47f095686a6
Muhd Fazri Mohd Faizal <muhdfazri920@gmail.com>
2023-06-21 14:12:37 +0800

    Buttong Buttong Fazereh

---
+++ resources/views/button_addtocartv1.php
@@ -0,0 +1,15 @@
+<head>
+    <link rel="stylesheet" href="/css/button_addtocartv1.scss.css" />
+</head>
+
+<button class="button">
+    <span>Add to cart</span>
+    <div class="cart">
+        <svg viewBox="0 0 36 26">
+            <polyline points="1 2.5 6 2.5 10 18.5 25.5 18.5 28.5 7.5 7.5 7.5"></polyline>
+            <polyline points="15 13.5 17 15.5 22 10.5"></polyline>
+        </svg>
+    </div>
+</button>
+
+<script type="text/javascript" src="/js/button_addtocartv1.js"></script>
---
+++ resources/views/title-logo.php
@@ -0,0 +1,12 @@
+<head>
+    <link rel="stylesheet" href="/css/title-logo.css" />
+</head>
+
+<main class="text-container">
+    <svg class="text-stroke" viewBox="0 10 650 100" width="100%" height="100%">
+        <text class="text" x="30" y="100">UKM OUTLET</text>
+    </svg>
+</main>
+<button class="reset">Restart the Animation</button>
+
+<script type="text/javascript" src="/js/title-logo.js"></script>
