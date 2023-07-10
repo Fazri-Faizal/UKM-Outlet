4c2ef8e1d88f2de8035c71700ea09e6e7e6e63e1
danial7101 <danial7101@gmail.com>
2023-07-10 17:37:17 +0800

    tolong buat faz

--- resources/views/search.php
+++ resources/views/search.php
@@ -20,13 +20,13 @@ $q = $_GET['ProductSearchBar'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/product-list-web.css"/>
+    <link rel="stylesheet" href="/css/product-list-search.css"/>
     <title><?php echo $_GET['ProductSearchBar']; ?> - Product List</title>
 </head>
 <body style="text-align:center;">
     <table class="product-list">
         <tr style="bgcolor: #556B2F;" >
-            <h2 style="color: #804444; face: verdana; text-align: center; padding-top: 20px; padding-bottom: 20px; position:relative; top:130px;">Showing result for '<?php echo $_GET['ProductSearchBar']; ?>' </h2>
+            <h2 style="color: #804444; face: verdana; text-align: center; padding-top: 20px; padding-bottom: 20px; ">Showing result for '<?php echo $_GET['ProductSearchBar']; ?>' </h2>
         </tr>
 
 <table class="product-list">
@@ -37,7 +37,7 @@ $q = $_GET['ProductSearchBar'];
                 <button onclick="window.location.href='product-details ? id=<?php $row['product_Id']?>'">
                     
                             <div class="card">
-                                <p style="margin-left: 250px"><svg xmlns="http://www.w3.org/2000/svg" style="margin-top: 20px;" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                <p style="margin-left: 250px"><svg xmlns="http://www.w3.org/2000/svg" style="margin-top: 20px; margin-right:5px;" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                                     </svg>
                                 </p>
