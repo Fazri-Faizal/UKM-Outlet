8d69608c06515a90aead194c7bd801cb90caa036
Aliasyahira01 <Aliasyahira0612@gmail.com>
2023-10-26 16:49:49 +0800

    user-profile frontend

--- resources/views/button_save.php
+++ resources/views/button_save.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="./css/button_save.css">
 </head>
 <body>
-    <div class="container">
+    <div>
         <button id="btn" class="custombtn">
             <p id="btnText">Submit</p>
             <div class="check-box">
@@ -24,6 +24,38 @@
 
         btn.onclick = () => {
             btnText.innerHTML = "Thanks";
+            btn.classList.add("active");
+        };
+    </script>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submit Button</title>
+    <link rel="stylesheet" href="./css/button_save.css">
+</head>
+<body>
+    <div>
+        <button id="btn" class="button-save" style="margin-left:80%; margin-bottom:20px">
+            <p id="btnText" style="margin-top: 13px;">Save</p>
+            <div class="check-box">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                    <path fill="transparent" class="icon-svg-path" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                </svg>
+            </div>
+        </button>
+    </div>
+    <script type="text/javascript">
+        const btn = document.querySelector("#btn");
+        const btnText = document.querySelector("#btnText");
+
+        btn.onclick = () => {
+            btnText.innerHTML = "Done !";
             btn.classList.add("active");
         };
     </script>
